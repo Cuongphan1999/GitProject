1b1825dbfed226b8cae4859dc9bdd6fa02b2331d
Cuongphan1999 <phanxuancuong00677@gmai.com>
2023-05-24 23:33:36 +0700

    done bai toan tu

---
+++ bai_toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Enter width');
+inputHeight = prompt('Enter height');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = height*width
+document.write('Area = ' + area);
