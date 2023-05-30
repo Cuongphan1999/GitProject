4416794981b5443c8514efa59f52e7ab806280cd
Cuongphan1999 <phanxuancuong00677@gmai.com>
2023-05-30 22:27:29 +0700

    done chu vi dien tich

---
+++ bai_chuvi_dientich.js
@@ -0,0 +1,8 @@
+
+    let r = prompt("enter bán kính hình tròn: ");
+    let chuvi = 2*r*Math.PI;
+    let dientich = Math.pow(r,2)*Math.PI;
+    document.write('Chuvi =' + chuvi); 
+    document.write('Dien tich ='+ dientich)
+
+
